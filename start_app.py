25485d5c180e3ef54333f6a06c82f3f7882cbc8b
Michael Rosenbaum <mick.rosenbaum@gmail.com>
2025-02-08 16:14:44 +0200

    "run from ECR"

--- start_app.py
+++ start_app.py
@@ -2,28 +2,23 @@ import os
 from dotenv import load_dotenv
 import subprocess
 
-# Load environment variables from the .env file into os.environ
 load_dotenv()
 
 
 def main():
-    # Pass all environment variables (including .env) explicitly
+
     env = os.environ.copy()
 
-    # Start the backend process with the full environment
     backend_process = subprocess.Popen(
         ["python", "backend/worker-service.py"], env=env
     )
 
-    # Start the frontend process with the full environment
     frontend_process = subprocess.Popen(
         ["python", "frontend/web-service.py"], env=env
     )
 
-    # Optionally, wait for processes to complete
     backend_process.communicate()
     frontend_process.communicate()
-
 
 if __name__ == "__main__":
     main()
